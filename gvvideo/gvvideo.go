27e9a7cfca103a7808f21b84661406d6fa5548bb
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-09-14 12:39:32 +0900

    feat: ReadFrameTo

--- gvvideo/gvvideo.go
+++ gvvideo/gvvideo.go
@@ -40,6 +40,58 @@ type GVVideo struct {
 	Reader            io.ReadSeeker
 }
 
+// ReadFrameTo decodes the specified frame into the provided RGBA buffer
+func (v *GVVideo) ReadFrameTo(frameID uint32, buf *image.RGBA) error {
+	if frameID >= v.Header.FrameCount {
+		return errors.New("end of video")
+	}
+	block := v.AddressSizeBlocks[frameID]
+	if _, err := v.Reader.Seek(int64(block.Address), io.SeekStart); err != nil {
+		return err
+	}
+	compressed := make([]byte, block.Size)
+	if _, err := io.ReadFull(v.Reader, compressed); err != nil {
+		return err
+	}
+	width := int(v.Header.Width)
+	height := int(v.Header.Height)
+	uncompressedSize := width * height * 4
+	decompressed := make([]byte, uncompressedSize)
+	if _, err := lz4.UncompressBlock(compressed, decompressed); err != nil {
+		return err
+	}
+	var fourCC string
+	switch v.Header.Format {
+	case GVFormatDXT1:
+		fourCC = "DXT1"
+	case GVFormatDXT3:
+		fourCC = "DXT3"
+	case GVFormatDXT5:
+		fourCC = "DXT5"
+	default:
+		return errors.New("unsupported format")
+	}
+	decoder, err := dxt.New(fourCC, width, height)
+	if err != nil {
+		return err
+	}
+	img, err := decoder.Decode(bytes.NewReader(decompressed))
+	if err != nil {
+		return err
+	}
+	switch src := img.(type) {
+	case *image.RGBA:
+		copy(buf.Pix, src.Pix)
+	case *image.NRGBA:
+		rgba := image.NewRGBA(src.Bounds())
+		draw.Draw(rgba, src.Bounds(), src, image.Point{}, draw.Src)
+		copy(buf.Pix, rgba.Pix)
+	default:
+		return errors.New("not RGBA image")
+	}
+	return nil
+}
+
 func ReadHeader(r io.Reader) (GVHeader, error) {
 	header := GVHeader{}
 	if err := binary.Read(r, binary.LittleEndian, &header.Width); err != nil {
@@ -102,52 +154,12 @@ func LoadGVVideo(path string) (*GVVideo, error) {
 
 // ReadFrame returns RGBA []uint8 for the specified frame
 func (v *GVVideo) ReadFrame(frameID uint32) ([]uint8, error) {
-	if frameID >= v.Header.FrameCount {
-		return nil, errors.New("end of video")
-	}
-	block := v.AddressSizeBlocks[frameID]
-	if _, err := v.Reader.Seek(int64(block.Address), io.SeekStart); err != nil {
-		return nil, err
-	}
-	compressed := make([]byte, block.Size)
-	if _, err := io.ReadFull(v.Reader, compressed); err != nil {
-		return nil, err
-	}
 	width := int(v.Header.Width)
 	height := int(v.Header.Height)
-	uncompressedSize := width * height * 4
-	decompressed := make([]byte, uncompressedSize)
-	if _, err := lz4.UncompressBlock(compressed, decompressed); err != nil {
-		return nil, err
-	}
-	var fourCC string
-	switch v.Header.Format {
-	case GVFormatDXT1:
-		fourCC = "DXT1"
-	case GVFormatDXT3:
-		fourCC = "DXT3"
-	case GVFormatDXT5:
-		fourCC = "DXT5"
-	default:
-		return nil, errors.New("unsupported format")
-	}
-	// DXT decode
-	decoder, err := dxt.New(fourCC, width, height)
+	buf := image.NewRGBA(image.Rect(0, 0, width, height))
+	err := v.ReadFrameTo(frameID, buf)
 	if err != nil {
 		return nil, err
 	}
-	img, err := decoder.Decode(bytes.NewReader(decompressed))
-	if err != nil {
-		return nil, err
-	}
-	if rgbaImg, ok := img.(*image.RGBA); ok {
-		return rgbaImg.Pix, nil
-	}
-	if nrgbaImg, ok := img.(*image.NRGBA); ok {
-		// Convert NRGBA to RGBA
-		rgba := image.NewRGBA(nrgbaImg.Bounds())
-		draw.Draw(rgba, nrgbaImg.Bounds(), nrgbaImg, image.Point{}, draw.Src)
-		return rgba.Pix, nil
-	}
-	return nil, errors.New("not RGBA image")
+	return buf.Pix, nil
 }
